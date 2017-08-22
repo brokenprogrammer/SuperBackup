5032e7e24dd34732c3408c18598c9282ae5a6e4c
Oskar Mendel <brokenprogrammer@gmail.com>
2017-08-22 19:11:50 +0200

    Initial project structure.

---
+++ SuperBackup/SuperBackup/main.cpp
@@ -0,0 +1,21 @@
+#include "stdio.h"
+
+int main()
+{
+	// Read in directories to watch, read in files to watch.
+	// Sit and watch directories, when save occurs, store the old file
+	// Back to watching directories.
+
+
+	// Example cfg file:
+	//SuperBackup.cfg
+	//directory = C:/my/test/app
+	//directory = C:/second/app
+	//filetype = *.c
+	//filetype = *.h
+	//filetype = *.cpp
+
+	printf("Hello, World!");
+
+	return 0;
+}
